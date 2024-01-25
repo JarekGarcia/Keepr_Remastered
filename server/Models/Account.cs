4507a73fb0d77ad872d3f867f3d8c25fdc13c1ec
JarekGarcia <prodijee@hotmail.com>
2024-01-25 11:42:48 -0700

    project initialized

---
+++ server/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace Keepr_Remastered.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
